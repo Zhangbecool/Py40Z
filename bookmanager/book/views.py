578c9c120527e5eaf11d14bf6517f7ea13477635
Zhangbecool <346657817@qq.com>
2020-08-14 20:24:37 +0800

    模板

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -4,5 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-
-    return HttpResponse('ok')
+    context = {
+        'name':'爱丽丝'
+    }
+    return render(request, 'book/index.html', context)
