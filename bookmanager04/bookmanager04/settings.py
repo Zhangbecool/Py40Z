3d63d4996326d9659c1c446db2eef057a9523b9a
Zhangbecool <346657817@qq.com>
2020-08-18 21:23:59 +0800

    redis存储session

--- bookmanager04/bookmanager04/settings.py
+++ bookmanager04/bookmanager04/settings.py
@@ -125,4 +125,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 
-SESSION_ENGINE='django.contrib.sessions.backends.db'
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_CACHE_ALIAS = 'default'
